a107fcfa2c5cd4734daa69ff0451183f7c6a4e15
Habibaudu <auduhabib1990@gmail.com>
2023-03-02 10:02:42 +0100

    line continuation character

---
+++ dailies.py
@@ -0,0 +1,7 @@
+# line continuation character
+
+print("My name is Habib and I am a python developer")
+print("My name is habib and \
+I am a python developer")
+
+#In python (\) is a line continuation character
